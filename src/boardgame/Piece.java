eb400645e4f72eebba8c4389660a1d6cda579eee
João Barbosa <contato.jsbarbosa@gmail.com>
2024-05-14 22:06:13 -0300

    Initial implement class: Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,19 @@
+package boardgame;
+
+public class Piece {
+    protected Position position;
+
+    private Board board;
+
+    // no construtorar não recebe a posição porque a posição inicial da peça é nulo
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+
+}
